47f23cf880ad290544fbce5470d3d9c2bc011911
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2024-08-15 21:19:00 -0300

    chore: add user id to session

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,16 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 
 import { db } from '@/lib/prisma';
 
+declare module 'next-auth' {
+  interface User {
+    id: string;
+  }
+
+  interface Session {
+    user: User;
+  }
+}
+
 const handler = NextAuth({
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
@@ -14,6 +24,16 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user = {
+        ...session.user,
+        id: user.id,
+      };
+
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
