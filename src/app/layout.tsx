55522e1025120794f9c40ab523090861e9b6211b
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2024-08-18 17:10:10 -0300

    feat: theme mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
 import './globals.css';
 
+import { ThemeProvider } from '@/providers/theme';
 import { AuthProvider } from '@/providers/auth';
 import { Toaster } from '@/components/ui/sonner';
 import { Header } from '@/components/header';
@@ -20,17 +21,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" className="dark">
+    <html lang="pt-br" suppressHydrationWarning>
       <body className={nunito.className}>
-        <AuthProvider>
-          <Header />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AuthProvider>
+            <Header />
 
-          {children}
+            {children}
 
-          <Footer />
+            <Footer />
 
-          <Toaster />
-        </AuthProvider>
+            <Toaster />
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { MenuIcon } from 'lucide-react';
 import { Button } from './ui/button';
+
+import { ThemeMode } from './theme-mode';
 import { Drawer } from './drawer';
 import { Logo } from './logo';
 
@@ -14,16 +16,20 @@ export function Header() {
   if (pathname.includes('barbershops/')) return;
 
   return (
-    <header className="flex items-center justify-between border-b p-5">
+    <header className="flex items-center justify-between gap-3 border-b p-5">
       <Link href="/" title="FSW Baber">
         <Logo height={20} width={120} />
       </Link>
 
-      <Drawer>
-        <Button size="icon" variant="ghost">
-          <MenuIcon className="size-5" />
-        </Button>
-      </Drawer>
+      <div className="flex items-center justify-center gap-3">
+        <ThemeMode />
+
+        <Drawer>
+          <Button size="icon" variant="ghost">
+            <MenuIcon className="size-5" />
+          </Button>
+        </Drawer>
+      </div>
     </header>
   );
 }
